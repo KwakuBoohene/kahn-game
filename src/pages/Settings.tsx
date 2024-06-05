cb24cf2cc3e804c689aff77b4e05dd2ed867cd40
Philip Owusu-Afriyie <50931622+pokaj@users.noreply.github.com>
2024-06-05 16:30:03 +0000

    turn on sound in settings - temp

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,12 +1,44 @@
+import letItGo from '../assets/music/let-it-go-12279.mp3'
+import {useEffect, useState} from "react";
+import settingsStore from '../store/settings'
 
 export default function Settings(){
+
+    const {settings, setSettings} = settingsStore()
+    const [audio] = useState(new Audio(letItGo))
+
+
+    // useEffect(() => {
+    //     if (settings.music){
+    //         audio.pause()
+    //         audio.currentTime = 0
+    //
+    //     } else {
+    //         audio.pause()
+    //     }
+    // }, [settings.music, audio])
+
+    const toggleMusic = async (status: boolean) => {
+        setSettings({...settings, music: status})
+        if (status){
+            try {
+                await audio.play()
+            } catch (error) {
+                console.error(`Error playing audio: ${error}`)
+            }
+        }else {
+            audio.pause()
+        }
+    }
+
     return (<div className="">
         <div className="flex justify-between my-2 items-center ">
             <span className="">Music</span>
             <span className="">
                 <span className="flex w-36 justify-between ">
-                    <button className="">On</button>
-                    <button className="">Off</button>
+
+                    <button className="" onClick={() => toggleMusic(true)}>On</button>
+                    <button className="" onClick={() => toggleMusic(false)}>Off</button>
                 </span>
             </span>
         </div>
