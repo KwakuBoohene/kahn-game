544924f6e9323f47e24fb96ee0a2ab6ba7d17449
kwaku.boohene <kwaku.kwayisi@gmail.com>
2024-07-27 23:24:37 +0000

    added deployment script for

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
-const express = require("express");
-const path = require("path");
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); 
 const app = express();
 app.use(express.static(__dirname + "/dist"));
 app.get("/*", function (req, res) {
