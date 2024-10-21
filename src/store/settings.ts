7f8eb8efdb1784e3e096a496290dbdb93543d6d8
kwaku.boohene <kwaku.kwayisi@gmail.com>
2024-10-21 01:16:22 +0000

    loaded music from settings

--- src/store/settings.ts
+++ src/store/settings.ts
@@ -1,11 +1,24 @@
 import { create } from 'zustand'
+import { devtools } from 'zustand/middleware';
+import letItGo from '../assets/music/let-it-go-12279.mp3'
 
-const settingsStore = create((set) => ({
-  settings: {
-    music : true,
-    sound_effects : true,
+export interface SettingStore {
+    settings:{
+      sound_effects: boolean,
+      audio: HTMLAudioElement
+    },
+    setSettings: (settings:{sound_effects:boolean}) => void
+}
+
+
+const settingsStore = (set:Function) => ({
+  settings:{
+    music: false,
+    sound_effects: false,
   },
-    setSettings: (settings:{music:boolean,sound_effects:boolean}) => set({ settings }),
-}))
+  audio: new Audio(letItGo),
+    setSettings: (settings:{sound_effects:boolean}) => set({settings})
+     
+})
 
-export default settingsStore;
+export const useSettingsStore = create(devtools(settingsStore));
